fcf342247623df44ec17d2bc1ca676a0ca4fd6ef
IsaiasCosta <isaiascssilva@gmail.com>
2018-05-24 13:28:02 -0300

    alteração fundamentos js

---
+++ array.js
@@ -0,0 +1,17 @@
+const array = [4.5, 5.9 , 1.6, 45, 98 ," Array "]
+console.log(array[0],array[3],array[2]);
+
+console.log(array.length)
+
+//Adicionar novos elementos no arrray Push
+array.push("string",{id:5}, 78.56, false)
+
+console.log(array)
+
+// remover um elemento do array com POP
+
+console.log(array.pop())
+
+delete array[5]
+console.log(array)
+console.log(typeof array)
