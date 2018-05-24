0fc248a95126cab23ac3f916650baafbf9458d2f
IsaiasCosta <isaiascssilva@gmail.com>
2018-05-24 13:32:28 -0300

    Alteração fundamentos_01

---
+++ boolean.js
@@ -0,0 +1,34 @@
+let ativo = false
+console.log(ativo)
+
+ativo = true
+console.log(ativo)
+
+//usando negação
+
+ativo = 1
+console.log(!ativo)
+
+//Tipos que são verdadeiros
+console.log(" Tipos Veradeiros")
+console.log(!!3)
+console.log(!!-1)
+console.log(!!'aaa')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(ativo = true))
+
+console.log("Tipos Falses")
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(ativo = false))
+console.log('Para inalizar ...')
+console.log(!!('' || null || 0 || 'ss'))
+ 
+
+
+
